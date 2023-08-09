49ddbc97180cad69f75b90e0c618bf1f8d56d5c6
gutaq1 <gutaq1@gmail.com>
2023-08-09 16:17:47 -0300

    aula 117

--- GUIAPRESS/articles/ArticlesController.js
+++ GUIAPRESS/articles/ArticlesController.js
@@ -97,14 +97,16 @@ router.get("/articles/page/:num", (req, res) => {
     if(isNaN(page) || page == 1){
         offset = 0;
     }else{
-        offset = parseInt(page) * 4;
+        offset = (parseInt(page) - 1) * 4;
     }
 
     Article.findAndCountAll({
         limit: 4,
-        offset: offset
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ], 
     }).then(articles => {
-
         var next;
         if(offset + 4 >= articles.count){
             next = false;
