96d8a0d54924869f469aa0cce7080305f455bb97
gutaq1 <gutaq1@gmail.com>
2023-10-26 17:31:57 -0300

    teste

--- GUIAPRESS/users/UsersController.js
+++ GUIAPRESS/users/UsersController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
-const { route } = require("express/lib/application");
+const bcrypt = require('bcryptjs');
 
 router.get("/admin/users", (req, res) =>{
     res.send("Listagem de usuários");
@@ -15,7 +15,19 @@ router.post("/users/create", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    res.json({email, password});
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/");
+    });
+
+    
 });
 
 module.exports = router;
