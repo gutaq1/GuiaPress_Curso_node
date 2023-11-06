ee03f29c9bc1658da96cb0b8d382482367dfbb72
gutaq1 <gutaq1@gmail.com>
2023-11-06 14:20:40 -0300

    121_lista de users

--- GUIAPRESS/users/UsersController.js
+++ GUIAPRESS/users/UsersController.js
@@ -4,7 +4,9 @@ const User = require("./User");
 const bcrypt = require('bcryptjs');
 
 router.get("/admin/users", (req, res) =>{
-    res.send("Listagem de usuários");
+   User.findAll().then(users => {
+    res.render("admin/users/index", {users: users});
+   });
 });
 
 router.get("/admin/users/create", (req,res) => {
